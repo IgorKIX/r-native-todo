279393c536214f57d33e19233423af9ac34b398f
IgorKIX <igorklo@pm.me>
2021-08-23 11:50:18 +0200

    Init commit

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+// Components
+import Task from "./components/Task";
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskList, setTaskList] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskList([...taskList, task]);
+    setTask(null);
+  };
+
+  const completeTask = (index) => {
+    let taskListCopy = [...taskList];
+    taskListCopy.splice(index, 1);
+    setTaskList(taskListCopy);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Tasks:</Text>
+        <View style={styles.items}>
+          {taskList.map((item, index) => {
+            return (
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Task text={item} />
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      </View>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Write a task"}
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -14,8 +67,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E8EAED",
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30,
+  },
+
+  // Add task input
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
   },
 });
---
+++ README.md
@@ -0,0 +1,19 @@
+# r-native-todo
+
+It's a simple todo app, just a sturtup project in React native world made with expo. Based on [this tutorial](https://www.youtube.com/watch?v=0kL6nhutjQ8).
+
+### Run project
+
+- [Set up the development environment](https://reactnative.dev/docs/environment-setup).
+
+Firs install dependencies: `yarn install` (or `npm install`).
+
+Then:
+
+- Run on Android: `yarn android` (or `npm run android`).
+
+- Run on iOS: `yarn ios` (or `npm run ios`).
+
+- Run on both Android & iOS: `yarn mobile` (or `npm run mobile`).
+
+You can also install the [Expo App](https://play.google.com/store/apps/details?id=host.exp.exponent&hl=en&gl=US) run the expo server and scan the QR code.
